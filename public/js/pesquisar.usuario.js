e855fa6cd705bb224f95237fe05622fb892ad17c
Dahan <Dahan@AJUADMNTB021.ifs.edu.br>
2019-10-28 17:03:50 -0300

    Adaptando modal de pesquisa de usuário

--- public/js/pesquisar.usuario.js
+++ public/js/pesquisar.usuario.js
@@ -1,67 +1,65 @@
+var datatableUsuarios;
+
 function popularTabelaPesquisarUsuario() {
-   return $('#tableUsuarios').DataTable({
+    datatableUsuarios = $('#tableUsuarios').DataTable({
         "fixedHeader": {
             header: true,
             footer: true
         },
-        "select" : {
-            style : 'single'
+        "select": {
+            style: 'single'
         },
-        "order": [[ 1, "asc" ]],
+        "order": [
+            [1, "asc"]
+        ],
         "language": {
-            "url": "<?= base_url() ?>/public/js/Portuguese.json"
+            "url": BASE_URL + "public/js/Portuguese.json"
         },
         ajax: {
             type: "POST",
-            url: "<?= base_url() ?>/app/src/controller/carregar/usuarios.php"
+            url: BASE_URL + "crudAjax/ajaxListarUsuarios",
+            success: function(response) {
+                preencherSelectUsuario(response.data)
+            }
         },
-        "processing" : true,
+        "processing": true,
         "columns": [
-            {data : "id"},
-            {data : "nome"},
-            {data : "telefone"},
-            {data : "email"},
-            {data : "tipo"},
-            {data : "cpf"},
-            {data : "matricula"}
+            { data: "id" },
+            { data: "nome" },
+            { data: "email" },
+            { data: "tipo" },
+            { data: "cpf" },
+            { data: "matricula" }
         ]
     });
 }
 
-function preencherSelectUsuario() {
-    $.ajax({
-    type: "POST",
-    url: '<?= base_url() ?>/app/src/controller/carregar/usuarios.php',
-    success: function(users) {
-            $.each(users.data, function (i, user) {
-                $('#selectUsuario').append($('<option>', { 
-                    value: user.id,
-                    text : user.nome + ' - ' + user.email 
-                }));
-            });
-        }
-    })
+function preencherSelectUsuario(usuarios) {
+    $.each(usuarios, function(i, usuario) {
+        $('#selectUsuario').append($('<option>', {
+            value: usuario.id,
+            text: usuario.nome + ' - ' + usuario.email
+        }));
+    });
 }
 
-function recuperarUsuarioSelecionado(tabelaUsuarios) {
-    if (tabelaUsuarios.rows( { selected: true }).count() === 0)
-        return 0; // não faz nada caso nenhum usuário seja selecionado na tabelaUsuarios
-    
-    // recupera a linha selecionada na tabelaUsuarios
-    var usuario = tabelaUsuarios.rows( { selected: true }).data()[0];
-    atualizarSelectUsuario(usuario);
+function recuperarUsuarioSelecionado() {
+    if (datatableUsuarios.rows({ selected: true }).count() === 0)
+        return false; // não faz nada caso nenhum usuário seja selecionado na tabelaUsuarios
+
+    // recupera as informações do usuário selecionado
+    var usuario = datatableUsuarios.rows({ selected: true }).data()[0];
+    atualizarSelectUsuario(usuario.id);
 }
 
-function atualizarSelectUsuario(usuario) {
-    $('#selectUsuario').val(usuario.id) // seleciona o usuário no selectUsuario
-    $('#selectUsuario').change();
-    $('#modalPesquisarUsuario').modal('hide');
+function atualizarSelectUsuario(id) {
+    // salva o id do usuário para enviar no formulário
+    $('#selectUsuario').val(id); // seleciona o usuário no selectUsuario
 }
 
-function configurarModalPesquisarUsuarios(tabelaUsuarios) {
-    $('#modalPesquisarUsuario').on('show.bs.modal', async function(){
-        await tabelaUsuarios.ajax.reload(); // atualiza a tabelaUsuarios ao abrir o modal
-        tabelaUsuarios.search( '' ).columns().search( '' ).draw(); // limpa o filtro de pesquisa
-        $('#modalPesquisarUsuario').modal('handleUpdate');
+function configurarModalPesquisarUsuarios() {
+    $('#modalPesquisarUsuario').on('show.bs.modal', async function() {
+        await datatableUsuarios.ajax.reload(); // atualiza a tabelaUsuarios ao abrir o modal
+        datatableUsuarios.search('').columns().search('').draw(); // limpa o filtro de pesquisa
     })
 }
