315a58e0f42880f25b67d36eb40dbab391d43b98
Dahan <Dahan@AJUADMNTB021.ifs.edu.br>
2019-10-28 17:04:21 -0300

    Adaptando escolha de cores (modal -> popover)

--- public/js/escolher.cores.js
+++ public/js/escolher.cores.js
@@ -1,73 +1,76 @@
+/**
+ * JavaScript responsável por selecionar as cores escolhidas no modalEscolherCor,
+ * criar um linear-gradient e adicioná-lo ao modal de cadastro/edição de bicicletas
+ */
 
-    var cores = ['#000000'] ;
+// Array que armazena as cores escolhidas
+// Preenchido por padrão com preto
+var cores = ['#000000'];
 
-    function adicionarOuRemoverCor(button, idDiv) {
-        let div = '#' + idDiv
-        if ($(div).hasClass('selecionada')) {
-            $(div).removeClass('selecionada')
-            $(button).html('Adicionar')
-        } else {
-            // let cores = $("#modalCorBody").find('.selecionada')
-            // if (cores.length >= 3) {
-            //     alert('Máximo de três cores!')
-            //     return;
-            // }
-            $(div).attr('class', $(div).attr('class') + ' selecionada')
-            $(button).html('Remover')
-        }
-    }
-
-    function selecionarCores() {
-
-        let divs = $("#modalCorBody").find('.selecionada')
-        cores = new Array ;
-        for (let i = 0; i < divs.length; i++) {
-            cores[i] = ('#'+$(divs[i]).attr('id'));
-            $(divs[i]).removeClass('selecionada')
-        }
-        pintarDivCores()
-
-        $('#modalEscolherCor').modal('hide');
-    }
-
-    function pintarDivCores() {
-        background = criarCssBackground()
-        $("#inputCor").css('background', background)
-    }
-
-    function atualizarArrayCores(novasCores) {
-        let novasCoresArray = novasCores.split(";")
-
-        // Remove a última posição caso seja vazia
-        // (Ocorre quando a string novasCores termina com ';')
-        if (novasCoresArray[novasCoresArray.length-1].length === 0 )
-            novasCoresArray.splice(novasCoresArray.length-1, 1)
-        cores = novasCoresArray;
+/**
+ * 
+ * @param {element} button botão que representa a cor escolhida no modal
+ * @param {string} idDiv id do container da cor escolhida
+ */
+function adicionarOuRemoverCor(button) {
+    if ($(button).hasClass('selecionada')) {
+        $(button).removeClass('selecionada')
+    } else {
+        /* Descomente as linhas abaixo para limitar a escolha de cores a apenas 3 (ou quantas desejar) */
+        // let cores = $("#modalCorBody").find('.selecionada')
+        // if (cores.length >= 3) {
+        //     alert('Máximo de três cores!')
+        //     return;
+        // }
+        $(button).addClass('selecionada')
     }
     
-    /**
-     * Cria um linear-gradient com as cores selecionadas no modal de escolha de cor
-     * var cores: Array definido como global neste script
-     */
-    function criarCssBackground() {
-        let background = 'repeating-linear-gradient(45deg, '
-        let quebraDeCor = 10 / cores.length * 10
-        let comprimento = 0
-        cores.forEach(cor => {
-            background += cor +' ' + comprimento +'% '  // #FFFFFF 0%
-            comprimento += quebraDeCor;                 // 0% += 50% = 50% 
-            background += comprimento +'%, '            // #FFFFFF 0% 50%, 
-        });
-        
-        background = background.substring(0, background.length - 2) + ')'
-        return background
+    selecionarCores();
+}
+
+/**
+ * Função responsável por preencher o array cores com as cores
+ * selecionadas e chamar a função que pinta a div que representa as cores
+ */
+function selecionarCores() {
+    let divs = $("#popoverCoresBody").find('.selecionada')
+    cores = new Array; // redefine o array de cores, removendo a cor padrão
+    for (let i = 0; i < divs.length; i++) {
+        // adiciona uma cor hexadecimal ao array de cores
+        // o id de cada cor é seu próprio valor
+        cores[i] = ('#' + $(divs[i]).attr('id')); 
     }
 
-    function stringArrayCores() {
-        let stringCores = ""
-        cores.forEach(cor => {
-            stringCores += cor + ';'
-        })
+    pintarDivCores()
+}
 
-        return stringCores
-    }
+/**
+ * Chama a função que cria o background CSS com as cores selecionadas
+ */
+function pintarDivCores() {
+    background = criarCssBackground()
+    $("#divCores").css('background', background) // atribui o backgroun à div que representa as cores
+    $("#inputCores").val(background) // altera o valor do input que será enviado pelo formulário
+}
+
+/**
+ * Cria um linear-gradient com as cores selecionadas no modal de escolha de cor
+ * var cores: Array definido como global neste script
+ */
+function criarCssBackground() {
+
+    if (cores.length == 0)
+        return 'black'
+
+    let background = 'repeating-linear-gradient(45deg, ';
+    let quebraDeCor = (10 / cores.length * 10) // define o tamanho de cada cor no gradiente
+    let comprimento = 0 // define o ponto onde cada cor irá terminar
+    cores.forEach(cor => {
+        background += cor + ' ' + comprimento + '% ' // #FFFFFF 0%
+        comprimento += quebraDeCor; // 0% + 50% = 50% 
+        background += comprimento + '%, ' // #FFFFFF 0% 50%, 
+    });
+
+    background = background.substring(0, background.length - 2) + ')'; // substitui a última vírgula pelo fechamento do gradiente
+    return background
+}
