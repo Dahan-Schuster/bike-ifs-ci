fef2fe5651bfbc88f0aa35c9c40aa6a3f56b6ae1
Dahan <Dahan@AJUADMNTB021.ifs.edu.br>
2019-10-28 17:04:43 -0300

    Página de listagem de bicicletas (#10)

--- bikeifs_app_folder/views/pages/listar/bicicletas.php
+++ bikeifs_app_folder/views/pages/listar/bicicletas.php
@@ -1,112 +1,132 @@
-<?php require_once(APPPATH.'models/ModeloBike.php'); ?>
+<?php require_once(APPPATH . 'models/ModeloBike.php'); ?>
 <div class="row">
-     <span class="col-12 col-md-6">
-         <h3>Lista de bicicletas</h3>
-     </span>
- </div>
- <hr class="my-3">
- <div class="table-responsive">
-     <table class="table table-sm responsive table-striped table-hover" id="tableBikes" style="width: 100%;">
-         <caption>
-             &nbsp;
-             <button data-toggle="modal" data-target="#modalSalvarBike" title="Cadastrar novo" type="button" class="btn btn-primary bmd-btn-fab bmd-btn-fab-sm">
-                 <i class="material-icons">add</i>
-             </button>
-             <button id="btnAtivarSelecionados" onclick="ativarBikesSelecionadas()" title="Ativar selecionadas" type="button" class="btn btn-info bmd-btn-fab bmd-btn-fab-sm">
-                 <i class="material-icons">thumb_up</i>
-             </button>
-             <button id="btnDestivarSelecionados" onclick="desativarBikesSelecionadas()" title="Desativar selecionadas" type="button" class="btn btn-danger bmd-btn-fab bmd-btn-fab-sm">
-                 <i class="material-icons">thumb_down</i>
-             </button>
-             <button id="btnSelecionarLinhas" title="Selecionar todos" type="button" class="btn accent-color bmd-btn-fab bmd-btn-fab-sm text-light">
-                 <i class="material-icons">check_box_outline_blank</i>
-             </button>
-         </caption>
-         <thead class="bg-default-primary">
-             <tr>
-                 <th>&#9432;</th>
-                 <th>Cor</th>
-                 <th>Modelo</th>
-                 <th>Marca</th>
-                 <th class="none">Obs</th>
-                 <th>Aro</th>
-                 <th>Dono</th>
-                 <th>Situacao</th>
-                 <th>Editar</th>
-             </tr>
-         </thead>
-         <tbody>
-         </tbody>
-     </table>
- </div>
- <br>
- <!-- Modal salvar bicicleta -->
- <div id="modalSalvarBike" class="modal fade" role="dialog">
-     <div class="modal-dialog modal-dialog-centered">
+    <span class="col-12 col-md-6">
+        <h3>Lista de bicicletas</h3>
+    </span>
+</div>
+<hr class="my-3">
+<div class="table-responsive">
+    <table class="table table-sm responsive table-striped table-hover" id="tableBikes" style="width: 100%;">
+        <caption>
+            &nbsp;
+            <button data-toggle="modal" data-target="#modalSalvarBike" data-backdrop="static" data-keyboard="false" title="Cadastrar novo" type="button" class="btn btn-primary bmd-btn-fab bmd-btn-fab-sm">
+                <i class="material-icons">add</i>
+            </button>
+            <button id="btnAtivarSelecionados" onclick="ativarBicicletasSelecionadas()" title="Ativar selecionadas" type="button" class="btn btn-info bmd-btn-fab bmd-btn-fab-sm">
+                <i class="material-icons">thumb_up</i>
+            </button>
+            <button id="btnDestivarSelecionados" onclick="desativarBicicletasSelecionadas()" title="Desativar selecionadas" type="button" class="btn btn-danger bmd-btn-fab bmd-btn-fab-sm">
+                <i class="material-icons">thumb_down</i>
+            </button>
+            <button id="btnSelecionarLinhas" title="Selecionar todos" type="button" class="btn accent-color bmd-btn-fab bmd-btn-fab-sm text-light">
+                <i class="material-icons">check_box_outline_blank</i>
+            </button>
+        </caption>
+        <thead class="bg-default-primary">
+            <tr>
+                <th>&#9432;</th>
+                <th>Cor</th>
+                <th>Modelo</th>
+                <th>Marca</th>
+                <th class="none">Obs</th>
+                <th>Aro</th>
+                <th>Dono</th>
+                <th>Situacao</th>
+                <th>Editar</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
+<br>
+<!-- Modal salvar bicicleta -->
+<div id="modalSalvarBike" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-dialog-centered">
 
-         <!-- Modal content-->
-         <div class="modal-content">
-             <div class="modal-header raised pb-3">
-                 <h3 class="modal-title">Salvar bicicleta</h3>
-                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-             </div>
-             <form id="formCadastroAdmin" autocomplete="off">
-                 <input type="hidden" id="id_bicicleta" name="id">
-                 <div class="modal-body">
-                     <div id="divInputCores" class="form-group">
-                         <label for="inputCores" class="bmd-label-floating">Cor</label>
-                         <input name="nome" type="hidden" id="inputCores">
-                         <div class="input-group">
-                             <div class="form-control bike-color"></div>
-                             <div class="input-group-append">
-                                 <button type="button" class="btn" data-toggle="modal" data-target="#modalEscolherCor">
-                                     <i class="material-icons">palette</i>
-                                 </button>
-                             </div>
-                         </div>
-                         <span class="invalid-feedback"></span>
-                     </div>
-                     <div id="divInputMarca" class="form-group">
-                         <label for="inputMarca" class="bmd-label-floating">Marca</label>
-                         <input name="marca" type="text" placeholder="Caloi, Shimano, Scott etc" class="form-control" id="inputMarca">
-                         <span class="invalid-feedback"></span>
-                     </div>
-                     <div id="divInputAro" class="form-group">
-                         <label for="inputAro" class="bmd-label-floating">Aro</label>
-                         <input name="aro" type="number" placeholder="20, 24, 26 etc" class="form-control" id="inputAro">
-                         <span class="invalid-feedback"></span>
-                     </div>
-                     <div id="divSelectModelo" class="form-group">
-                         <label for="selectModelo" class="bmd-label-floating">Modelo</label>
-                         <select name="modelo" class="form-control" id="selectModelo">
-                             <option value="<?= ModeloBike::URBANA ?>">Urbana</option>
-                             <option value="<?= ModeloBike::DOBRAVEL ?>">Dobrável</option>
-                             <option value="<?= ModeloBike::FIXA ?>">Fixa</option>
-                             <option value="<?= ModeloBike::MOUNTAIN ?>">Mountain</option>
-                             <option value="<?= ModeloBike::SPEED ?>">Speed</option>
-                             <option value="<?= ModeloBike::BMX ?>">BMX</option>
-                             <option value="<?= ModeloBike::DOWNHILL ?>">Downhill</option>
-                             <option value="<?= ModeloBike::ELETRICA ?>">Elétrica</option>
-                         </select>
-                         <span class="invalid-feedback"></span>
-                     </div>
-                     <div id="divSelectUsuario" class="form-group">
-                         <label for="selectUsuario" class="bmd-label-floating">Repita a senha</label>
-                         <select name="id_usuario" class="form-control" id="selectUsuario">
-                         </select>
-                         <span class="invalid-feedback"></span>
-                     </div>
-                 </div>
-                 <div class="modal-footer">
-                     <button type="button" class="btn btn-secondary mr-auto" data-dismiss="modal">Cancelar</button>
-                     <button id="btnEnviar" type="submit" class="btn btn-primary">Cadastrar</button>
-                 </div>
-             </form>
-         </div>
-     </div>
- </div>
- <!-- Fim modal salvar bicicleta -->
- <!---------------------->
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header raised pb-3">
+                <h3 class="modal-title">Salvar bicicleta</h3>
+                <button type="button" class="close closePopover" data-dismiss="modal">&times;</button>
+            </div>
+            <form id="formSalvarBike" autocomplete="off">
+                <div class="modal-body">
+                    <input type="hidden" id="idBicicleta" name="id">
+                    <div id="divInputCores" class="form-group">
+                        <label for="inputCores" class="bmd-label-floating mb-0 pb-0">Cor</label>
+                        <input name="cores" type="hidden" id="inputCores">
+                        <div class="input-group">
+                            <div id="divCores" class="form-control bike-color">
+                                <i class="material-icons">directions_bike</i>
+                            </div>
+                            <div class="input-group-append">
+                                <button type="button" class="btn" id="btnPalette">
+                                    <i class="material-icons">palette</i>
+                                </button>
+                            </div>
+                        </div>
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div class="form-row">
+                        <div id="divInputMarca" class="form-group col-12 col-sm-4">
+                            <label for="inputMarca" class="bmd-label-floating mb-3">Marca</label>
+                            <input name="marca" type="text" placeholder="Caloi, Shimano etc" class="form-control" id="inputMarca">
+                            <span class="invalid-feedback"></span>
+                        </div>
+                        <div id="divInputAro" class="form-group col-12 col-sm-4">
+                            <label for="inputAro" class="bmd-label-floating mb-3">Aro</label>
+                            <input name="aro" type="number" placeholder="20, 24, 26 etc" class="form-control" id="inputAro">
+                            <span class="invalid-feedback"></span>
+                        </div>
+                        <div id="divSelectModelo" class="form-group col-12 col-sm-4">
+                            <label for="selectModelo" class="bmd-label-floating">Modelo</label>
+                            <select name="modelo" class="form-control" id="selectModelo">
+                                <option value="">Selecione</option>
+                                <option value="<?= ModeloBike::URBANA ?>">Urbana</option>
+                                <option value="<?= ModeloBike::DOBRAVEL ?>">Dobrável</option>
+                                <option value="<?= ModeloBike::FIXA ?>">Fixa</option>
+                                <option value="<?= ModeloBike::MOUNTAIN ?>">Mountain</option>
+                                <option value="<?= ModeloBike::SPEED ?>">Speed</option>
+                                <option value="<?= ModeloBike::BMX ?>">BMX</option>
+                                <option value="<?= ModeloBike::DOWNHILL ?>">Downhill</option>
+                                <option value="<?= ModeloBike::ELETRICA ?>">Elétrica</option>
+                            </select>
+                            <span class="invalid-feedback"></span>
+                        </div>
+                    </div>
+                    <div id="divInputObs" class="form-group">
+                        <label for="inputObs" class="bmd-label-floating">Observações sobre a bicicleta</label>
+                        <textarea name="obs" class="form-control" id="inputObs"></textarea>
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div id="divSelectUsuario" class="form-group">
+                        <label for="selectUsuario" class="bmd-label-floating">
+                            Pesquise um usuário clicando no botão ao lado
+                        </label>
+                        <div class="input-group">
+                            <select class="form-control" id="selectUsuario" name="id_usuario">
+                                <option value="">Selecione o dono da bicicleta</option>
+                            </select>
+                            <div class="input-group-append">
+                                <button type="button" class="btn" data-toggle="modal" data-target="#modalPesquisarUsuario">
+                                    <i class="material-icons">search</i>
+                                </button>
+                            </div>
+                        </div>
+                        <span class="invalid-feedback"></span>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary mr-auto closePopover" data-dismiss="modal">Cancelar</button>
+                    <button id="btnEnviar" type="submit" class="btn btn-primary closePopover">Salvar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- Fim modal salvar bicicleta -->
+<!---------------------->
 
- <?php include_once('public/views/modals/modalEscolherCor.html'); ?>
- <script language="JavaScript" src="<?= base_url() ?>/public/js/escolher.cores.js"></script>
+<?php include_once('public/views/modals/popperEscolherCores.html'); ?>
+<?php include_once('public/views/modals/modalPesquisarUsuario.html'); ?>
--- public/js/pages/bicicletas.js
+++ public/js/pages/bicicletas.js
@@ -1,32 +1,210 @@
 var datatable;
-var botaoEditar = `<button type="button" onclick="excluirAdmin(this)" class="btn btn-primary bmd-btn-icon">
-                        <i class="material-icons">edit</i>
-                    </button>`;
+
+// Configuração da página
+$(document)
+    .ready(() => {
+        popularTabela();
+        popularTabelaPesquisarUsuario();
+        setInterval(function() {
+            datatable.ajax.reload();
+        }, 120000); // atualiza a tabela a cada 2 minutos
+
+        // Configura o botão selecionar todos (o resto da configuração encontra-se no util.js de forma genérica)
+        configurarBotaoSelecionarLinhas(
+            document.getElementById('btnSelecionarLinhas'),
+            '#tableBikes',
+            datatable)
+
+        configurarPopoverCores();
+
+        // Reseta o formulário e os erros do modal de cadastro ao abrir
+        $('#modalSalvarBike')
+            .on('show.bs.modal', function() {
+                clearErrors();
+                $('#formSalvarBike')
+                    .trigger('reset')
+                $('#formSalvarBike')
+                    .find('#idBicicleta')
+                    .val('')
+                $('#formSalvarBike')
+                    .find('#inputCores')
+                    .val('black')
+                $('#formSalvarBike')
+                    .find('#divCores')
+                    .css('background', 'black')
+                $('#formSalvarBike')
+                    .find('#selectUsuario')
+                    .val('')
+                $('#popoverEscolherCores')
+                    .find('#popoverCoresBody')
+                    .find('.selecionada')
+                    .removeClass('selecionada')
+            })
 
 
-$(document).ready(function() {
-    popularTabela();
-    setInterval(function() {
-        datatable.ajax.reload();
-    }, 120000); // atualiza a tabela a cada 2 minutos
+        // Ativa o menu 'Listar' na navbar
+        $(".nav-link")
+            .removeClass('active')
+        $("#navLinkListagem")
+            .addClass('active')
 
+    });
 
-});
+// Cadastro/Edição de bicicletas
+$("#formSalvarBike")
+    .submit(function(form) {
+        form.preventDefault()
 
-// Métodos de controle (edição, ativar/desativar, carregamento da tabela)
+        $.ajax({
+            type: 'POST',
+            url: BASE_URL + 'crudAjax/ajaxSalvarBicicleta',
+            dataType: 'json',
+            data: $(this)
+                .serialize(),
+            beforeSend: function() {
+                $("#btnEnviar")
+                    .html(loadingImg('Validando dados...'))
+            },
+            success: function(response) {
+                if (response['status'] == 1) {
+                    datatable.ajax.reload()
+                    swal.fire('Sucesso!', 'Bicicleta cadastrada com sucesso. Aguarde a atualização da tabela.', 'success')
+                } else {
+                    showErrors(response['error_list'])
+                }
+            },
+            complete: function() {
+                $("#btnEnviar")
+                    .html('Salvar')
+            }
+        })
 
-function editar(button) {
-    var data = recuperarInformacoesDaBike(button);
-    atualizarCamposDoModalEditar(data);
-    $('#modalEditar').modal('show');
-}
+        return false;
+    })
 
-function alterarSituacao(bike, situacao) {
+// Ativar/desativar bicicletas
+
+function alterarSituacaoBicicleta(bike, situacao) {
+    var ids_bicicletas = [bike];
     if (situacao == 'Ativa')
-        desativar(bike)
+        enviarAjaxDesativar(ids_bicicletas)
     else if (situacao == 'Inativa')
-        ativar(bike)
+        enviarAjaxAtivar(ids_bicicletas)
 }
+
+function ativarBicicletasSelecionadas() {
+    var ids_bicicletas = []
+    datatable.rows({ selected: true })
+        .data()
+        .toArray()
+        .forEach((row) => {
+            ids_bicicletas.push(row.bikes.id)
+        })
+
+    if (ids_bicicletas.length == 0) {
+        swal.fire("Ativar selecionados", "Nenhuma linha selecionada. Selecione uma bike clicando em sua linha na tabela.", "error")
+    } else {
+        swal.fire({
+                type: 'warning',
+                title: "Ativar " + ids_bicicletas.length + " bicicleta(s) ",
+                text: 'Deseja realmente ativar as bicicletas selecionadas?',
+                showCancelButton: true,
+                cancelButtonText: 'Não',
+                confirmButtonText: 'Sim, ativar',
+                confirmButtonColor: 'crimson'
+            })
+            .then((querAtivar) => {
+                if (querAtivar.value)
+                    enviarAjaxAtivar(ids_bicicletas)
+            })
+    }
+}
+
+function desativarBicicletasSelecionadas() {
+    var ids_bicicletas = []
+    datatable.rows({ selected: true })
+        .data()
+        .toArray()
+        .forEach((row) => {
+            ids_bicicletas.push(row.bikes.id)
+        })
+
+    if (ids_bicicletas.length == 0) {
+        swal.fire("Desativar selecionados", "Nenhuma linha selecionada. Selecione uma bike clicando em sua linha na tabela.", "error")
+    } else {
+        swal.fire({
+                type: 'warning',
+                title: "Desativar " + ids_bicicletas.length + " bicicleta(s) ",
+                text: 'Deseja realmente desativar os bicicletas selecionados?',
+                showCancelButton: true,
+                cancelButtonText: 'Não',
+                confirmButtonText: 'Sim, desativar',
+                confirmButtonColor: 'crimson'
+            })
+            .then((querDesativar) => {
+                if (querDesativar.value)
+                    enviarAjaxDesativar(ids_bicicletas)
+            })
+    }
+}
+
+function enviarAjaxAtivar(ids_bicicletas) {
+    $.ajax({
+        type: 'POST',
+        url: BASE_URL + 'crudAjax/ajaxAtivarBicicletas',
+        dataType: 'json',
+        data: { ids_bicicletas },
+        beforeSend: function() {
+            $("#btnAtivarSelecionados")
+                .html(loadingImg())
+        },
+        success: function(response) {
+            if (response['status'] == 1) {
+                swal.fire("Sucesso!", "Bicicleta(s) ativada(s) com sucesso.", "success")
+            } else {
+                swal.fire("Erro", response['error_message'], "error")
+            }
+            datatable.ajax.reload()
+        },
+        error: function(response) {
+            console.log(response)
+        },
+        complete: function() {
+            $("#btnAtivarSelecionados")
+                .html('<i class="material-icons">thumb_up</i>')
+        }
+    })
+}
+
+function enviarAjaxDesativar(ids_bicicletas) {
+    $.ajax({
+        type: 'POST',
+        url: BASE_URL + 'crudAjax/ajaxDesativarBicicletas',
+        dataType: 'json',
+        data: { ids_bicicletas },
+        beforeSend: function() {
+            $("#btnDestivarSelecionados")
+                .html(loadingImg())
+        },
+        success: function(response) {
+            if (response['status'] == 1) {
+                swal.fire("Sucesso!", "Bicicleta(s) desativada(s) com sucesso.", "success")
+            } else {
+                swal.fire("Erro", response['error_message'], "error")
+            }
+            datatable.ajax.reload()
+        },
+        error: function(response) {
+            console.log(response)
+        },
+        complete: function() {
+            $("#btnDestivarSelecionados")
+                .html(' <i class="material-icons">thumb_down</i>')
+        }
+    })
+}
+
+// Listagem de bicicletas
 
 function popularTabela() {
     datatable = $('#tableBikes').DataTable({
@@ -36,6 +214,9 @@ function popularTabela() {
         "order": [
             [6, "asc"]
         ],
+        'select': {
+            'style': 'multi'
+        },
         "columnDefs": [{
                 // Centraliza o conteúdo das colunas referentes aos botões
                 "className": "dt-center",
@@ -51,7 +232,7 @@ function popularTabela() {
                 // O correto funcionamento depende de que a cor esteja em formado hexadecimal (#000000 -
                 // - #ffffff), o que é garantido pelo formulário de cadastro de bicicletas. 
                 "render": function(cores, type, row) {
-                    let modelo = row.bikes.modelo.toLowerCase()
+                    let modelo = row.bikes.nome_modelo.toLowerCase()
                     let output = `<div onclick="abrirPainelLateralBike(${row.bikes.id})" 
                                 class="bike-color" style="background: ${cores};">`
                     output += `<img src="${BASE_URL}public/img/icons/bike-${modelo}.png" title="Bike" alt=""></div>`
@@ -79,7 +260,7 @@ function popularTabela() {
                     if (situacao == 'Ativa')
                         checked = 'checked'
 
-                    return `<input onchange="alterarSituacao('${row.bikes.id}',${situacao})" 
+                    return `<input onchange="alterarSituacaoBicicleta('${row.bikes.id}','${situacao}')" 
                                 type="checkbox" class="custom-switch hidden" id="switchSituacao${row.bikes.id}" ${checked}>
                             <label class="custom-switch-label" for="switchSituacao${row.bikes.id}"></label>`;
                 },
@@ -127,7 +308,7 @@ function popularTabela() {
                 data: "bikes.cores"
             },
             {
-                data: "bikes.modelo"
+                data: "bikes.nome_modelo"
             },
             {
                 data: "bikes.marca"
@@ -146,7 +327,12 @@ function popularTabela() {
             },
             {
                 "render": function() {
-                    return botaoEditar;
+                    return `<button class="btn btn-primary bmd-btn-icon" onclick='atualizarCamposModal(this)'
+                                    data-toggle="modal" data-target="#modalSalvarBike" 
+                                    data-backdrop="static" data-keyboard="false" 
+                                    title="Editar" type="button">
+                    <i class="material-icons">edit</i>
+                </button>`;
                 }
             }
         ]
@@ -157,36 +343,47 @@ function popularTabela() {
 
 // Métodos chamados por outros métodos
 
-function recuperarInformacoesDoModalEditar() {
-    var id = $('#modalEditar').find('.modal-footer').find('#bike').val();
-    var cores = stringArrayCores();
-    var marca = $('#modalEditar').find('.modal-body').find('#inputMarca').val();
-    var obs = $('#modalEditar').find('.modal-body').find('#inputObs').val();
-    var aro = $('#modalEditar').find('.modal-body').find('#inputAro').val();
-    var id_usuario = $('#modalEditar').find('.modal-body').find('#idUsuario').val();
+function configurarPopoverCores() {
+    var popover = $("#popoverEscolherCores")
+    var button = $('#btnPalette')
+    button.on('click', () => {
+        popover.toggle()
+        var popper = new Popper(button, popover, {
+            placement: 'right',
+            modifiers: {
+                flip: {
+                    behavior: ['left', 'right', 'top', 'bottom']
+                },
+                arrow: {
+                    enabled: true
+                }
+            }
+        })
+    })
 
-    var dados = '{ "id" : "' + id + '", "cores" : "' + cores + '", "marca" : "' + marca + '", "obs" : "' + obs + '", "aro" : "' + aro + '", "id_usuario" : "' + id_usuario + '"}';
-    return JSON.parse(dados);
+    $('.closePopover').click(() => {
+        popover.hide()
+    })
 }
 
-function atualizarCamposDoModalEditar(data) {
-    let id = data.id;
-    let coresBike = data.cores;
-    let marca = data.marca;
-    let obs = data.obs;
-    let aro = data.aro;
-    let id_usuario = data.id_usuario;
+function atualizarCamposModal(botao) {
 
-    atualizarArrayCores(coresBike);
-    pintarDivCores();
-    $('#modalEditar').find('.modal-body').find('#inputMarca').val(marca);
-    $('#modalEditar').find('.modal-body').find('#inputObs').val(obs);
-    $('#modalEditar').find('.modal-body').find('#inputAro').val(aro);
-    $('#modalEditar').find('.modal-body').find('#idUsuario').val(id_usuario);
+    var bike = recuperarInformacoesDaLinhaDatatable(botao, datatable).bikes;
 
-    $('#modalEditar').find('.modal-footer').find('#bike').val(id);
-}
+    let id = bike.id;
+    let cores = bike.cores;
+    let marca = bike.marca;
+    let aro = bike.aro;
+    let modelo = bike.modelo;
+    let obs = bike.obs;
+    let id_usuario = bike.id_usuario;
 
-function recuperarInformacoesDaBike(button) {
-    return recuperarInformacoesDaLinha(button).bikes;
+    $('#modalSalvarBike').find('.modal-body').find('#idBicicleta').val(id);
+    $('#modalSalvarBike').find('.modal-body').find('#inputCores').val(cores);
+    $('#modalSalvarBike').find('.modal-body').find('#divCores').css('background', cores);
+    $('#modalSalvarBike').find('.modal-body').find('#inputMarca').val(marca);
+    $('#modalSalvarBike').find('.modal-body').find('#inputObs').val(obs);
+    $('#modalSalvarBike').find('.modal-body').find('#inputAro').val(aro);
+    $('#modalSalvarBike').find('.modal-body').find('#selectModelo').val(modelo);
+    $('#modalSalvarBike').find('.modal-body').find('#selectUsuario').val(id_usuario);
 }
