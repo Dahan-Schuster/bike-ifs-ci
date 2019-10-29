084d33989eeec7965a1eb75ac24187b66faf915e
Dahan <Dahan@AJUADMNTB021.ifs.edu.br>
2019-10-29 00:16:11 -0300

    Página de listagem de Tags RFID (#10)

--- bikeifs_app_folder/views/pages/listar/tags.php
+++ bikeifs_app_folder/views/pages/listar/tags.php
@@ -1,247 +1,108 @@
-<?php
-@session_start();
-if (isset($_SESSION['login'])) {
-    if ($_SESSION['tipoAcesso'] == 'admin' || $_SESSION['tipoAcesso'] == 'funcionario') { ?>
-        <div class="row">
-            <span class="col-12 col-md-6">
-                <h1>Lista de tags RFID</h1>
-            </span>
-            <div class="col-md-3"></div>
-            <a class="col-11 col-md-3 btn btn-success mu-0 mb-3 mx-auto" href="?pagina=listagem" role="button">
-                Voltar para o menu de listagem
-            </a>
-        </div>
-        <hr class="my-3">
-        <div class="table-responsive">
-            <table class="table table-sm responsive bg-light table-bordered table-striped table-hover" id="tableTags" style="width: 100%;">
-                <caption>Lista de tags RFID</caption>
-                <thead class="table-h">
-                    <tr>
-                        <th>&#9432;</th>
-                        <th>Codigo</th>
-                        <th>Bike</th>
-                        <th class="desktop">Modelo</th>
-                        <th>Marca</th>
-                        <th>Aro</th>
-                        <th>Situação Bicicleta</th>
-                        <th>Dono</th>
-                        <th class="desktop">Apagar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                </tbody>
-                <tfoot class="table-f">
-                    <tr>
-                        <th>&#9432;</th>
-                        <th>Codigo</th>
-                        <th>Bike</th>
-                        <th>Modelo</th>
-                        <th>Marca</th>
-                        <th>Aro</th>
-                        <th>Situação Bicicleta</th>
-                        <th>Dono</th>
-                        <th>Apagar</th>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-        <br>
-    <?php } else { ?>
-        <div class="alert alert-danger" role="alert">
-            Você não tem permissão para acessar esta página.
-        </div>
-    <?php }
-    } else { ?>
-    <div class="alert alert-danger" role="alert">
-        É necessário fazer login para acessar esta página.
-    </div>
-<?php } ?>
-<!-- Modal excluir -->
-<div id="modalExcluir" class="modal fade" role="dialog">
+<div class="row">
+    <span class="col-12 col-md-6">
+        <h3>Lista de tags RFID</h3>
+    </span>
+</div>
+<hr class="my-3">
+<div class="table-responsive">
+    <table class="table table-sm responsive table-striped table-hover" id="tableTags" style="width: 100%;">
+        <caption>
+            &nbsp;
+            <button data-toggle="modal" data-target="#modalCadastroTag" data-backdrop="static" data-keyboard="false" title="Cadastrar novo" type="button" class="btn btn-primary bmd-btn-fab bmd-btn-fab-sm">
+                <i class="material-icons">add</i>
+            </button>
+            <button id="btnExcluirSelecionados" onclick="excluirTagsSelecionadas()" title="Excluir selecionados" type="button" class="btn btn-danger bmd-btn-fab bmd-btn-fab-sm">
+                <i class="material-icons">delete</i>
+            </button>
+            <button id="btnSelecionarLinhas" title="Selecionar todos" type="button" class="btn accent-color bmd-btn-fab bmd-btn-fab-sm text-light">
+                <i class="material-icons">check_box_outline_blank</i>
+            </button>
+        </caption>
+        <thead class="bg-default-primary">
+            <tr>
+                <th>&#9432;</th>
+                <th>Codigo</th>
+                <th>Bike</th>
+                <th>Modelo</th>
+                <th>Marca</th>
+                <th>Aro</th>
+                <th>Situação Bicicleta</th>
+                <th>Dono</th>
+                <th>Excluir</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
+<br>
+<!-- Modal Cadastrar Tag-->
+<div id="modalCadastroTag" class="modal fade" role="dialog">
     <div class="modal-dialog modal-dialog-centered">
 
         <!-- Modal content-->
         <div class="modal-content">
-            <div class="modal-header alert alert-danger">
-                <h3 class="modal-title">Remover etiqueta</h3>
+            <div class="modal-header raised pb-3">
+                <h3 class="modal-title">Cadastrar nova Tag RFID</h3>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
-            <div class="modal-body">
-                <h4 class="mt-0">Está certo de que deseja apagar esta etiqueta?</h4>
-                <h5>Você estará removendo-a do sistema, possibilitando um futuro recadastramento da mesma com outras informações.</h5>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary mr-auto" data-dismiss="modal">Cancelar</button>
-                <button type="button" class="btn btn-warning" id="tag" onclick="confirmarExclusao(this);">Sim</button>
-            </div>
+            <form autocomplete="off" id="formCadastroTag">
+                <div class="modal-body">
+                    <div id="divInputUid" class="form-group">
+                        <label for="inputUid" class="bmd-label-floating">
+                            Clique no botão ao lado para ler uma Tag RFID
+                        </label>
+                        <div class="input-group">
+                            <input name="codigo" type="text" placeholder="Código UID" class="form-control" id="inputUid">
+                            <div class="input-group-append">
+                                <button type="button" class="btn" data-toggle="modal" data-target="#modalLerTag">
+                                    <img src="<?= base_url('public/img/icons/rfid.png') ?>" class="img-responsive" title="Ler Tag RFID" alt="Ler Tag Rfid">
+                                </button>
+                            </div>
+                        </div>
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div id="divSelectUsuario" class="form-group">
+                        <label for="selectUsuario" class="bmd-label-floating">
+                            Pesquise um usuário clicando no botão ao lado
+                        </label>
+                        <div class="input-group">
+                            <select class="form-control" id="selectUsuario">
+                                <option value="">Selecione o dono da bicicleta</option>
+                            </select>
+                            <div class="input-group-append">
+                                <button type="button" class="btn" data-toggle="modal" data-target="#modalPesquisarUsuario">
+                                    <i class="material-icons">search</i>
+                                </button>
+                            </div>
+                        </div>
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div id="divSelectBicicleta" class="form-group">
+                        <label for="selectBicicleta" class="bmd-label-floating">
+                            Selecione a bicicleta que portará a Tag RFID
+                        </label>
+                        <select class="form-control" id="selectBicicleta" name="id_bicicleta">
+                            <option value="">Primeiramente, selecione um usuário.</option>
+                        </select>
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="selectedBikeColor" class="bmd-label-floating">Cor da bicicleta selecionada</label>
+                        <div id="selectedBikeColor" class="form-control bike-color">
+                            <i class="material-icons">directions_bike</i>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary mr-auto" data-dismiss="modal">Cancelar</button>
+                    <button id="btnEnviarCadastro" type="submit" class="btn btn-primary btn-raised">Cadastrar</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
-<!-- Fim modal excluir -->
-<!---------------------------->
-<script language="JavaScript" src="<?= base_url() ?>/public/js/escolher.cores.slim.js"></script>
-<script type="text/javascript">
-    var tabela;
-    var botaoExcluir = `<a onclick="excluir(this);" class="link btn btn-danger excluir">
-                            <img src="<?= base_url() ?>/public/img/icons/delete.png" title="Excluir bicicleta" alt="Excluir">
-                        </a>`;
-
-    $(document).ready(function() {
-        popularTabela();
-        setInterval(function() {
-            tabela.ajax.reload();
-        }, 120000); // atualiza a tabela a cada 2 minutos
-
-        
-    });
-
-    function excluir(button) {
-        var data = recuperarInformacoesDaLinha(button);
-        var id = data.tags.id;
-
-        $('#modalExcluir').find('.modal-footer').find('#tag').val(id);;
-        $('#modalExcluir').modal('show');
-    }
-
-    function confirmarExclusao(button) {
-
-        var id = button.value;
-        $.ajax({
-            type: "POST",
-            url: "<?= base_url() ?>/app/src/controller/excluir/etiqueta.php",
-            data: {
-                "id": id
-            },
-            success: function(res) {
-                if (res == 'success') {
-                    alertSnackBar($("#alertaSucesso"), 'Exclusão realizada com sucesso!')
-                    tabela.ajax.reload();
-                } else if (res == 'error_1')
-                    alertSnackBar($("#alertaInvalido"), 'Ocorreu um erro durante a exclusão. Tente novamente mais tarde.')
-            }
-        });
-        $('#modalExcluir').modal('hide');
-    }
-
-    function popularTabela() {
-        tabela = $('#tableTags').DataTable({
-            "fixedHeader": {
-                footer: true
-            },
-            "columnDefs": [{
-                    // Centraliza o conteúdo das colunas referentes aos botões
-                    "className": "dt-center",
-                    "targets": '_all'
-                },
-                {
-                    // Remove a opção 'ordenar' das colunas referentes aos botões
-                    "orderable": false,
-                    "targets": -1
-                },
-                {
-                    // Altera a coluna referente à cor da bike para uma div com a respectiva cor.
-                    // O correto funcionamento depende de que a cor esteja em formado hexadecimal (#000000 -
-                    // - #ffffff), o que é garantido pelo formulário de cadastro de bicicletas. 
-                    "render": function(cores, type, row) {
-                        atualizarArrayCores(cores);
-                        cssBackground = criarCssBackground();
-                        
-                        let modelo = row.bikes.modelo.toLowerCase()
-                        let output = `<div onclick="abrirPainelLateralBike(`+ row.bikes.id +`)" 
-                                class="bike-color" style="background: ` + cssBackground + `;">`
-                        output += '<img src="<?= base_url() ?>/public/img/icons/bike-' + modelo + '.png" title="Bike" alt=""></div>'
-                        return output
-                    },
-                    "targets": 2 // Coluna referente à cor.
-                },
-                {
-                    // Estiliza a coluna referente à situação do usuário
-                    "render": function(situacao) {
-                        var badge_class = 'd-none'; // por padrão, esconde a identificação de tipo antes de verificar se é válido
-
-                        if (situacao === 'Ativa')
-                            badge_class = 'badge-success';
-                        else if (situacao === 'Inativa')
-                            badge_class = 'badge-danger';
-
-                        return '<span class="badge ' + badge_class + '"><h6><b>' + situacao + '</b></h6></span>';
-                    },
-                    "targets": 6
-                },
-                {
-                    // Adiciona um link para o perfil de cada usuário
-                    "render": function(nome, type, row) {
-                        let output = '<div class="tooltip-w3 tooltip-w3-dotted">' 
-                        output += '<span onclick="abrirPerfilLateralUsuario('+row.users.id+')">'+ nome.split(" ")[0]+'</span>'
-                        output += '<span class="tooltiptext-w3">'
-                        output += 'Clique para ver mais'
-                        output += '</span>'
-                        return output
-                    },
-                    "targets": 7 // Coluna referente ao nome.
-                },
-                {
-                    "width": "15%",
-                    "targets": 2
-                }, // Garante que a coluna da cor terá um tamanho adequado
-
-                // Define a ordem de prioridade de visibilidade de cada coluna
-                {
-                    responsivePriority: 10001,
-                    targets: 7
-                },
-                {
-                    responsivePriority: 10002,
-                    targets: 3
-                },
-            ],
-            "language": {
-                "url": "<?= base_url() ?>/public/js/Portuguese.json"
-            },
-            'processing': true,
-            ajax: {
-                type: "POST",
-                url: "<?= base_url() ?>/app/src/controller/carregar/etiquetas.php"
-            },
-            "columns": [{
-                    data: "tags.id"
-                },
-                {
-                    data: "tags.codigo"
-                },
-                {
-                    data: "bikes.cores"
-                },
-                {
-                    data: "bikes.modelo"
-                },
-                {
-                    data: "bikes.marca"
-                },
-                {
-                    data: "bikes.aro"
-                },
-                {
-                    data: "bikes.situacao"
-                },
-                {
-                    data: "users.nome"
-                },
-                {
-                    "render": function() {
-                        return botaoExcluir;
-                    }
-                }
-            ]
-        });
-    }
-
-    function recuperarInformacoesDaLinha(button) {
-        var linha = $(button).parents('tr');
-        if (linha.hasClass('child')) { // Verifica se o botão está dentro de uma div expansível (para telas pequenas)
-            linha = linha.prev(); // Caso esteja, aponta para a linha anterior (a linha mãe)
-        }
-        var data = tabela.row(linha).data();
-        return data;
-    }
-</script>
+<!-- Fim Modal Cadastrar Tag -->
+<?php include_once('public/views/modals/modalPesquisarUsuario.html'); ?>
+<?php include_once('public/views/modals/modalLerTag.html'); ?>
---
+++ public/js/pages/tags.js
@@ -0,0 +1,307 @@
+var datatable;
+
+$(document).ready(function() {
+    popularTabela();
+    popularTabelaPesquisarUsuario();
+    configurarModalLerTag();
+    configurarSelectBicicleta();
+
+    setInterval(function() {
+        datatable.ajax.reload();
+    }, 120000); // atualiza a tabela a cada 2 minutos
+
+    // Máscara do UID da tag RFID no formulário de cadastro
+    $("#inputUid")
+        .mask('ZZ ZZ ZZ ZZ', {
+            translation: {
+                'Z': {
+                    pattern: /[0-9A-F]/,
+                    optional: false
+                }
+            }
+        });
+
+    // Ativa o menu 'Listar' na navbar
+    $(".nav-link")
+        .removeClass('active')
+    $("#navLinkListagem")
+        .addClass('active')
+
+    // Configura o botão selecionar todos (o resto da configuração encontra-se no util.js de forma genérica)
+    configurarBotaoSelecionarLinhas(
+        document.getElementById('btnSelecionarLinhas'),
+        '#tableTags',
+        datatable)
+
+    // Reseta o formulário e os erros do modal de cadastro ao abrir
+    $('#modalCadastroTag')
+        .on('show.bs.modal', function() {
+            clearErrors();
+            $('#formCadastroTag')
+                .trigger('reset')
+        })
+
+});
+
+$("#formCadastroTag")
+    .submit(function(form) {
+        form.preventDefault()
+
+        $.ajax({
+            type: 'POST',
+            url: BASE_URL + 'crudAjax/ajaxSalvarTagRFID',
+            dataType: 'json',
+            data: $(this)
+                .serialize(),
+            beforeSend: function() {
+                $("#btnEnviarCadastro")
+                    .html(loadingImg('Validando dados...'))
+            },
+            success: function(response) {
+                if (response['status'] == 1) {
+                    datatable.ajax.reload()
+                    swal.fire('Sucesso!', 'Tag cadastrada com sucesso. Aguarde a atualização da tabela.', 'success')
+                } else {
+                    showErrors(response['error_list'])
+                }
+            },
+            complete: function() {
+                $("#btnEnviarCadastro")
+                    .html('Cadastrar')
+            }
+        })
+    })
+
+
+function excluirTag(botao) {
+    let id = recuperarInformacoesDaLinhaDatatable(botao, datatable).tags.id
+    let ids_tags = [id]
+
+    swal.fire({
+            type: 'warning',
+            title: 'Excluir Tag RFID',
+            text: 'Deseja realmente excluir a tag RFID selecionada? Ao ser deletada, a tag estará livre para ser recadastrada com outra bicicleta. De preferência, certifique-se de ter o consentimento do dono atual.',
+            showCancelButton: true,
+            cancelButtonText: 'Não',
+            confirmButtonText: 'Sim, excluir',
+            confirmButtonColor: 'crimson'
+        })
+        .then((querExcluir) => {
+            if (querExcluir.value)
+                enviarAjaxExclusao(ids_tags)
+        })
+}
+
+function excluirTagsSelecionadas() {
+    var ids_tags = []
+    datatable.rows({ selected: true })
+        .data()
+        .toArray()
+        .forEach((row) => {
+            ids_tags.push(row.tags.id)
+        })
+
+    if (ids_tags.length == 0) {
+        swal.fire("Excluir selecionados", "Nenhuma linha selecionada. Selecione uma Tag RFID clicando em sua linha na tabela.", "error")
+    } else {
+        swal.fire({
+                type: 'warning',
+                title: "Excluir " + ids_tags.length + " tag(s) RFID",
+                text: 'Deseja realmente excluir as Tags selecionadas? Após a exclusão, as tags estarão livres para serem recadastradas com outras bicicletas. De preferência, certifique-se de ter o consentimento de todos os donos.',
+                showCancelButton: true,
+                cancelButtonText: 'Não',
+                confirmButtonText: 'Sim, excluir',
+                confirmButtonColor: 'crimson'
+            })
+            .then((querExcluir) => {
+                if (querExcluir.value)
+                    enviarAjaxExclusao(ids_tags)
+            })
+    }
+}
+
+function enviarAjaxExclusao(ids_tags) {
+    $.ajax({
+        type: 'POST',
+        url: BASE_URL + 'crudAjax/ajaxDeletarTagsRFID',
+        dataType: 'json',
+        data: { ids_tags },
+        beforeSend: function() {
+            $("#btnExcluirSelecionados")
+                .html(loadingImg())
+        },
+        success: function(response) {
+            if (response['status'] == 1) {
+                swal.fire("Sucesso!", "Tags RFID excluídas com sucesso. Lembre-se: apenas o código UID da Tag foi removida do sistema. Ela pode agora ser recadastrada apontando para outra bicicleta, se for preciso.", "success")
+            } else {
+                swal.fire("Erro", response['error_message'], "error")
+            }
+            datatable.ajax.reload()
+        },
+        error: function(response) {
+            console.log(response)
+        },
+        complete: function() {
+            $("#btnExcluirSelecionados")
+                .html(' <i class="material-icons">delete</i>')
+        }
+    })
+}
+
+function popularTabela() {
+    datatable = $('#tableTags').DataTable({
+        "fixedHeader": {
+            footer: true
+        },
+        'select': {
+            style: 'multi'
+        },
+        "columnDefs": [{
+                // Centraliza o conteúdo das colunas referentes aos botões
+                "className": "dt-center",
+                "targets": '_all'
+            },
+            {
+                // Remove a opção 'ordenar' das colunas referentes aos botões
+                "orderable": false,
+                "targets": -1
+            },
+            {
+                // Altera a coluna referente à cor da bike para uma div com a respectiva cor.
+                // O correto funcionamento depende de que a cor esteja em formado hexadecimal (#000000 -
+                // - #ffffff), o que é garantido pelo formulário de cadastro de bicicletas. 
+                "render": function(cores, type, row) {
+
+                    let modelo = row.bikes.nome_modelo.toLowerCase()
+                    let output = `<div onclick="abrirPainelLateralBike(${row.bikes.id})" 
+                            class="bike-color" style="background: ${cores};">`
+                    output += `<img src="${BASE_URL}public/img/icons/bike-${modelo}.png" title="Bike" alt=""></div>`
+                    return output
+                },
+                "targets": 2 // Coluna referente à cor.
+            },
+            {
+                // Estiliza a coluna referente à situação da bicicleta
+                "render": function(situacao) {
+                    var badge_class = 'd-none'; // por padrão, esconde a identificação de tipo antes de verificar se é válido
+
+                    if (situacao === 'Ativa')
+                        badge_class = 'badge-success';
+                    else if (situacao === 'Inativa')
+                        badge_class = 'badge-danger';
+
+                    return '<span class="badge ' + badge_class + '"><h6><b>' + situacao + '</b></h6></span>';
+                },
+                "targets": 6
+            },
+            {
+                // Adiciona um link para o perfil de cada usuário
+                "render": function(nome, type, row) {
+                    let output = '<div class="tooltip-w3 tooltip-w3-dotted">'
+                    output += '<span onclick="abrirPerfilLateralUsuario(' + row.users.id + ')">' + nome.split(" ")[0] + '</span>'
+                    output += '<span class="tooltiptext-w3">'
+                    output += 'Clique para ver mais'
+                    output += '</span>'
+                    return output
+                },
+                "targets": 7 // Coluna referente ao nome.
+            },
+            {
+                "width": "15%",
+                "targets": 2
+            }, // Garante que a coluna da cor terá um tamanho adequado
+
+            // Define a ordem de prioridade de visibilidade de cada coluna
+            {
+                responsivePriority: 10001,
+                targets: 7
+            },
+            {
+                responsivePriority: 10002,
+                targets: 3
+            },
+        ],
+        // Configura o comportamento do botão '+' (mais informações)
+        // O padrão, caso nenhuma cofiguração seja feita, é expandir a linha para baixo
+        // Esta configuração abre um modal ao invés disso
+        "responsive": {
+            details: {
+                // Abre um modal com as informações da linha
+                display: $.fn.dataTable.Responsive.display.modal({
+                    header: function(linha) {
+                        return 'Tag RFID # ' + linha.data().tags.codigo
+                    }
+                }),
+                renderer: $.fn.dataTable.Responsive.renderer.tableAll({
+                    tableClass: 'table'
+                })
+            }
+        },
+        "language": {
+            "url": BASE_URL + 'public/js/Portuguese.json'
+        },
+        'processing': true,
+        ajax: {
+            type: "POST",
+            url: BASE_URL + 'crudAjax/ajaxListarTagsRFID'
+        },
+        "columns": [{
+                data: "tags.id"
+            },
+            {
+                data: "tags.codigo"
+            },
+            {
+                data: "bikes.cores"
+            },
+            {
+                data: "bikes.nome_modelo"
+            },
+            {
+                data: "bikes.marca"
+            },
+            {
+                data: "bikes.aro"
+            },
+            {
+                data: "bikes.situacao"
+            },
+            {
+                data: "users.nome"
+            },
+            {
+                "render": function() {
+                    return `<button onclick="excluirTag(this);" class="btn btn-danger bmd-btn-icon">
+                                <i class="material-icons">delete</i>
+                            </button>`;
+                }
+            }
+        ]
+    });
+}
+
+function configurarSelectBicicleta() {
+    // Atualizar select bicicleta ao escolher um usuário
+    $('#selectUsuario').change(function() {
+        var id_usuario = $(this).val();
+        if (id_usuario) {
+            $.ajax({
+                type: 'POST',
+                url: BASE_URL + 'crudAjax/gerarOpcoesDeBikesPorUsuario',
+                data: { id_usuario },
+                success: function(html) {
+                    $('#selectBicicleta').html(html);
+                }
+            });
+        } else {
+            $('#selectBicicleta').html('<option value="">Primeiramente, selecione um usuário.</option>');
+        }
+    });
+
+    // Atualizar o a div cores ao escolher uma bicicleta
+    $('#selectBicicleta').change(function() {
+        var cores = $('#selectBicicleta option:selected').data('color');
+        if (!cores) cores = '#fff'
+        $("#selectedBikeColor").css('background', cores)
+    })
+}
