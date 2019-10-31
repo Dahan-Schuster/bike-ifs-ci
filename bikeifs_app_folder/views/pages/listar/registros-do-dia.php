f5b7b00cd34395ce460bcade55189180594950ad
Danielly Silva <daniellyfanisz@gmail.com>
2019-10-31 17:37:13 -0300

    Página de listagem de registros do dia (#10)

--- bikeifs_app_folder/views/pages/listar/registros-do-dia.php
+++ bikeifs_app_folder/views/pages/listar/registros-do-dia.php
@@ -1,13 +1,15 @@
 <div class="row">
-    <span class="col-6">
+    <span class="col-12 col-md-6">
         <h3>Registros do dia</h3>
     </span>
     <?php if ($pode_registrar) : ?>
-        <button data-backdrop="static" class="btn btn-danger col-3" type="button" data-toggle="modal" data-target="#modalRegistroManual">
-            <img src="<?= base_url() ?>/public/img/icons/rfid.png" class="img-responsive" title="Ler Tag RFID" alt="Ler Tag Rfid">
+        <button data-backdrop="static" class="btn col-12 col-md-3" type="button" data-toggle="modal" data-target="#modalRegistroManual">
+            <span class="mr-3">Registrar entrada</span>
+            <i class="material-icons">add</i>
         </button>
-        <button class="btn btn-danger col-3" type="button" data-toggle="modal" data-target="#modalLerTag">
-            <img src="<?= base_url() ?>/public/img/icons/rfid.png" class="img-responsive" title="Ler Tag RFID" alt="Ler Tag Rfid">
+        <button class="btn col-12 col-md-3" type="button" data-toggle="modal" data-target="#modalLerTag">
+            <span class="mr-3">Ler Tag Rfid</span>
+            <img src="<?= base_url() ?>/public/img/icons/rfid.png" class="img-responsive ml-3" title="Ler Tag RFID" alt="Ler Tag Rfid">
         </button>
     <?php endif; ?>
 </div>
@@ -36,9 +38,118 @@
         </tbody>
     </table>
 </div>
+<!-- Modal registrar entrada -->
+<div id="modalRegistroManual" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-dialog-centered">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header raised pb-3">
+                <h3 class="modal-title">Registrar entrada</h3>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <form id="formRegistrarEntradaManual">
+                <div class="modal-body">
+                    <div id="divSelectUsuario" class="form-group">
+                        <label for="selectUsuario" class="bmd-label-floating">
+                            Pesquise um usuário clicando no botão ao lado
+                        </label>
+                        <div class="input-group">
+                            <select class="form-control" id="selectUsuario">
+                                <option value="">Selecione o dono da bicicleta</option>
+                            </select>
+                            <div class="input-group-append">
+                                <button type="button" class="btn" data-toggle="modal" data-target="#modalPesquisarUsuario">
+                                    <i class="material-icons">search</i>
+                                </button>
+                            </div>
+                        </div>
+                        <span class="invalid-feedback"></span>
+                    </div>
+                    <div class="form-row">
+                        <div id="divSelectBicicleta" class="form-group col-12 col-sm-6">
+                            <label for="selectBicicleta" class="bmd-label-floating">
+                                Selecione a bicicleta
+                            </label>
+                            <select class="form-control" id="selectBicicleta" name="id_bicicleta">
+                                <option value="">Primeiramente, selecione um usuário.</option>
+                            </select>
+                            <span class="invalid-feedback"></span>
+                        </div>
+                        <div class="form-group col-12 col-sm-6">
+                            <label for="selectedBikeColor" class="bmd-label-floating">Cor da bicicleta selecionada</label>
+                            <div id="selectedBikeColor" class="form-control bike-color">
+                                <i class="material-icons">directions_bike</i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputObs">Observações sobre a entrada</label>
+                        <textarea class="form-control" id="inputObs" name="obs" maxlength="255"></textarea>
+                    </div>
+                    <div id="divInputNumTrava" class="form-group">
+                        <label for="inputNumTrava">Número do cadeado utilizado (0 para nenhum)</label>
+                        <input type="number" class="form-control" id="inputNumTrava" name="num_trava" min="0" max="15" value="0">
+                        <span class="invalid-feedback"></span>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary mr-auto" data-dismiss="modal">Cancelar</button>
+                    <button id="btnCheckinManual" type="submit" class="btn btn-primary btn-raised"><i>Check-in</i></button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- Fim modal registrar entrada -->
+<!--------------------------------->
+<!-- Modal registro automático -->
+<div id="modalRegistroAutomatico" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-dialog-centered">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header alert alert-info">
+                <h3 class="modal-title">Confirme as informações</i></h3>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="inputUID">UID</label>
+                    <span id="inputUID" class="form-control mt-1"></span>
+                </div>
+                <div class="form-group">
+                    <label for="divBicicleta">Bicicleta</label>
+                    <div id="divBicicleta" class="form-control bike-color">
+                        <img src="<?= base_url() ?>/public/img/icons/bycicle.png" title="Bike" alt="">
+                    </div>
+                    <input type="hidden" id="bikeId" />
+                </div>
+                <div class="form-group">
+                    <label for="spanUsuario">Usuário - Matrícula</label>
+                    <span id="spanUsuario" class="form-control mt-1"></span>
+                </div>
+                <hr>
+                <div class="form-group">
+                    <label for="inputObs">Observações sobre a entrada</label>
+                    <textarea class="form-control" id="inputObs" name="obs" maxlength="255"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="inputNumTrava">Número do cadeado utilizado (0 para nenhum)</label>
+                    <input type="number" class="form-control" id="inputNumTrava" name="num_trava" min="0" max="15" value="0">
+                </div>
+            </div>
+            <input type="hidden" id="idUsuario">
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary mr-auto" data-dismiss="modal">Cancelar</button>
+                <button type="button" onclick="realizarRegistroAutomatico()" class="btn btn-primary"><i>Check-in</i></button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Fim Modal registro automático -->
 
 <?php
-include_once('public/views/modals/modalsRegistro.php');
 include_once('public/views/modals/modalPesquisarUsuario.html');
 include_once('public/views/modals/modalLerTag.html');
 ?>
--- public/js/pages/registros-do-dia.js
+++ public/js/pages/registros-do-dia.js
@@ -1,16 +1,13 @@
 var datatable;
-var botaoRealizarCheckout = `<a onclick="confirmarCheckOut(this);" class="btn btn-danger text-light">
+var botaoRealizarCheckout = `<button onclick="confirmarCheckOut(this);" class="btn btn-danger bmd-btn-fab bmd-btn-fab-sm">
                                 <i class="material-icons">send</i>
-                            </a>`;
+                            </button>`;
 
-var botaoCheckoutRealizado = `<a onclick="confirmarCheckOut(this);" class="btn btn-success">
+var botaoCheckoutRealizado = `<button onclick="alertarCheckoutRealizado()" class="btn btn-success bmd-btn-icon">
                                     <i class="material-icons">check</i>
-                            </a`;
+                            </button`;
 
-// TODO: Listagem de registros por dia
 // TODO: Inserção automática de registros (RFID)
-// TODO: Checkout de registros
-// TODO: Desfazer checkout
 
 $(document)
     .ready(function() {
@@ -27,7 +24,9 @@ $(document)
     });
 
 
-// Ação do formulário de Checkin
+/**
+ * Ação do formulário de Checkin
+ */
 $("#formRegistrarEntradaManual").submit(function(form) {
     form.preventDefault();
 
@@ -45,8 +44,11 @@ $("#formRegistrarEntradaManual").submit(function(form) {
             if (response['status'] == 1) {
                 atualizarDataTable(null, datatable)
                 snackBarSucesso()
+                fecharModal($("#modalRegistroManual"));
             } else if (response['status'] == -1) {
-                swal.fire('Permissão negada', 'Administradores não possuem permissão para relizar registros.', 'error')
+                swal.fire('Permissão negada', response['error_message'], 'error')
+            } else if (response['status'] == -2) {
+                alertarBicicletaOuUsuarioInativos(response['objetos'])
             } else {
                 showErrors(response['error_list'])
             }
@@ -60,25 +62,107 @@ $("#formRegistrarEntradaManual").submit(function(form) {
     return false;
 });
 
+/**
+ * Abre um sweetalert com dois modais para vericação dos dados da bicicleta
+ * e para envio de observações sobre o checkout.
+ * No final, chama a função para envio da requisição Ajax de checkout
+ * 
+ * @param {HTMLButtonElement} botao o botão checkout, usado para recuperar os dados da linha    
+ */
+function confirmarCheckOut(botao) {
+    var data = recuperarInformacoesDaLinhaDatatable(botao, datatable)
+    Swal.mixin({
+        confirmButtonText: 'Avançar &rarr;',
+        cancelButtonText: 'Cancelar',
+        showCancelButton: 'Cancelar',
+        progressSteps: ['1', '2']
+    }).queue([{
+            title: 'Check-Out',
+            html: `<h4>Confirme as informações da bicicleta</h4>
+                    <table class="table table-borderless">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th colspan="4">Bicicleta</th>
+                            </tr>
+                            <tr>
+                                <th>Modelo</th>
+                                <th>Marca</th>
+                                <th>Cor</th>
+                                <th>Aro</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>${data.bikes.modelo}</td>
+                                <td>${data.bikes.marca}</td>
+                                <td><div class="col-3 bike-color" style="max-width: 100%; background: ${data.bikes.cores}"></div></td>
+                                <td>${data.bikes.aro}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="table table-borderless">
+                        <thead class="thead-light">
+                            <tr>
+                                <th colspan="4">Usuário (dono)</th>
+                            </tr>
+                            <tr>
+                                <th>Nome</th>
+                                <th>CPF</th>
+                                <th>Matrícula</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>${data.users.nome}</td>
+                                <td>${data.users.cpf}</td>
+                                 <td>${data.users.matricula}</td>
+                            </tr>
+                        </tbody>
+                    </table>`
+        },
+        {
+            title: 'Deseja registrar alguma observação?',
+            input: 'textarea',
+        }
+    ]).then((result) => {
+        if (result.value) {
+            enviarAjaxCheckout(result.value[1], data.registros.id)
+        }
+    })
+}
 
-// Ação do formulário de Checkout
-$('#formCheckOut').submit(function(form) {
-    form.preventDefault();
-
-    return false;
-
-});
-
-function desfazerCheckout(regId) {
+function enviarAjaxCheckout(obs, id_registro) {
     $.ajax({
         type: "POST",
-        url: BASE_URL + "",
+        url: BASE_URL + "crudAjax/ajaxCheckoutRegistro",
+        dataType: 'json',
         data: {
-            "id": regId
+            obs,
+            id_registro
         },
-        success: function(res) {
-            if (res == 'success') {
+        success: function(response) {
+            console.log(response)
+            if (response['status'] == 1) {
+                atualizarDataTable(null, datatable)
+                snackBarSucessoCheckout(response['id_registro'], response['id_saida'])
+            }
+        }
+    })
+}
 
+function desfazerCheckout(id_registro, id_saida) {
+    $.ajax({
+        type: "POST",
+        url: BASE_URL + "crudAjax/ajaxDesfazerCheckout",
+        dataType: 'json',
+        data: {
+            id_registro,
+            id_saida
+        },
+        success: function(response) {
+            if (response['status'] == 1) {
+                atualizarDataTable(null, datatable)
+                snackBarSucessoCheckoutDesfeito()
             }
         }
     })
@@ -208,7 +292,7 @@ function popularTabelaRegistros(timestamp) {
             },
             {
                 "render": function(data, type, row) {
-                    if (row.saidas.hora == 'Pendente')
+                    if (row.saidas.data_hora == 'Pendente')
                         return botaoRealizarCheckout;
                     return botaoCheckoutRealizado;
                 }
@@ -217,6 +301,9 @@ function popularTabelaRegistros(timestamp) {
     });
 }
 
+/**
+ * Configura o comportamento dos modais de inserção de registros
+ */
 function configurarModalCadastroRegistro() {
     $("#modalRegistroManual").on('show.bs.modal', function() {
         $(this).find('form').trigger('reset')
@@ -229,7 +316,9 @@ function configurarModalCadastroRegistro() {
     })
 }
 
-// Selecionar usuário e bicicleta no modal de cadastro de registros
+/**
+ * Configura o comportamento dos selects Usuário e Bicicleta no modal de inserção de registros
+ */
 function configurarSelectBicicleta() {
 
     // Atualizar select bicicleta ao escolher um usuário
@@ -250,10 +339,10 @@ function configurarSelectBicicleta() {
     });
 
     // Atualizar a div cores ao escolher uma bicicleta
-    $('#selectBicicleta').change(function() {
+    $('#modalRegistroManual').find('#selectBicicleta').change(function() {
         var cores = $('#selectBicicleta option:selected').data('color');
         if (!cores) cores = '#fff'
-        $("#selectedBikeColor").css('background', cores)
+        $('#modalRegistroManual').find("#selectedBikeColor").css('background', cores)
     })
 
 }
@@ -289,22 +378,101 @@ function criarEConfigurarSelectData() {
         change: function(e) {
             datatable.destroy()
             let data = $(selectData).val().substring(6) + '-' + $(selectData).val().substring(3, 5) + '-' + $(selectData).val().substring(0, 2)
-            popularTabelaRegistros(Date.parse(data))
+            popularTabelaRegistros(getTimeStampAtual(data))
         }
     })
 }
 
+/**
+ * Confira o input de texto que é preenchido após a leitura de uma tag RFID
+ * para chamar a função que irá pesquisar por uma bicicleta através do UID
+ * da Tag assim que o evento UidDetectado for disparado.
+ * 
+ * Este evento é disparado pelo método preencherInputUID no arquivo ler.tag.js
+ * 
+ */
 function configurarRegistroAutomatico() {
     $('#inputUID').on('UidDetectado', function() {
         pesquisarBikePorUID($('#inputUID').html());
     });
 }
 
-// Função chamada pelo botão de pesquisa de usuários
-// Chamar este método em conjunto com o evento de abertura do modal
-// melhora a experiência de visualização da troca de modais.
-// Esconder o modal de registros após abrir o modal de pesquisa não
-// fica bonito de se ver. Dessa forma ficou mais agradável.
-function esconderModalRegistro() {
-    $('#modalRegistroManual').modal('hide');
+/**
+ * Abre um sweetalert informando já fora realizado um checkout para o registro escolhido.
+ */
+function alertarCheckoutRealizado() {
+    swal.fire('Checkout Realizado', 'Esta bicicleta já teve sua saída registrada. Verifique se selecionou a bicicleta correta.', 'warning')
+}
+
+/**
+ * Abre um sweetalert contento um aviso sobre a situação da Bicicleta e do Usuário.
+ * Um pequeno painel de controle é gerado para edição rápida dos objetos, podendo ativá-los
+ * antes de realizar o registro de entrada.
+ * 
+ * @param {array} objetos resposta do servidor contendo os objetos Bicicleta e Usuário para edição
+ */
+function alertarBicicletaOuUsuarioInativos(objetos) {
+    swal.fire({
+        type: 'warning',
+        allowOutsideClick: false,
+        title: 'Permissão negada',
+        html: `Um ou mais problemas foram encontrados com a bicicleta e o usuário. Por favor, verifique os erros abaixo e tome as devidas providências.
+                <hr><div class="row d-fler justify-content-center">
+                <div class="row col-12">
+                <span class="col-6">Situação da bike</span> <input 
+                    type="checkbox" class="custom-switch hidden" id="switchSituacaoBike" ${objetos.bicicleta.situacao == 0 ? 'checked' : ''}>
+                <label class="custom-switch-label" for="switchSituacaoBike"></label>
+                </div>
+                <div class="row col-12">
+                <span class="col-6">Situação do usuário</span> <input 
+                    type="checkbox" class="custom-switch hidden" id="switchSituacaoUser" ${objetos.usuario.situacao == 0 ? 'checked' : ''}>
+                <label class="custom-switch-label" for="switchSituacaoUser"></label>
+                </div>`, // TODO: bike verificada/não verificada
+        showCancelButton: true,
+        confirmButtonText: 'Enviar e Registrar entrada',
+        cancelButtonText: 'Enviar'
+    }).then((querRegistrar) => {
+        if ($('#switchSituacaoBike').is(':checked')) {
+            enviarAjaxAtivarBicicletas([objetos.bicicleta.id])
+        }
+        if ($('#switchSituacaoUser').is(':checked')) {
+            enviarAjaxAtivarUsuarios([objetos.usuario.id])
+        }
+        if (querRegistrar.value)
+            $('#btnCheckinManual').click()
+        else
+            swal.fire('Sucesso', 'Operação realizada com sucesso. Tente registrar a entrada novamente', 'success')
+    })
+}
+
+/**
+ * Mostra um snackbar com um botão para desfazer o checkout recém-registrado
+ * 
+ * @param {int} id_registro id do registro associado à saída
+ * @param {int} id_saida id da saída recém-registrada
+ */
+function snackBarSucessoCheckout(id_registro, id_saida) {
+    var options = {
+        content: `<i class='material-icons'>check_circle_outline</i>
+                <span class='mb-5'>Checkout realizado com sucesso</span>
+                <button onclick="desfazerCheckout(${id_registro}, ${id_saida})" type="button" class="btn btn-primary"><strong>Desfazer</strong></button>`,
+        style: "snackbar",
+        timeout: 5000
+    }
+
+    $.snackbar(options);
+}
+
+/**
+ * Mostra um snackbar informando que a o checkout foi desfeito
+ */
+function snackBarSucessoCheckoutDesfeito() {
+    var options = {
+        content: `<i class='material-icons'>check_circle_outline</i>
+                <span class='mb-5'>Checkout desfeito com sucesso</span>`,
+        style: "snackbar",
+        timeout: 5000
+    }
+
+    $.snackbar(options);
 }
